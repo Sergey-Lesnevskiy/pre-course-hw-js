a13dcefdaa2dab0286240344cd78ffe9006adbd9
Lesnevsky-Sergey <78030028+Sergey-Lesnevskiy@users.noreply.github.com>
2024-05-29 11:30:36 +0300

    fix:5-11

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -1,6 +1,4 @@
 
-// Дан объект passport. Создай его копию и поменяй имя с "Petr" на "Ivan", и убедись в консоли, что у объекта passport имя осталось "Petr", а у твоего нового объекта "Ivan".
-
 let passport = {
     name: "Petr",
     surname: "Petrov",
@@ -12,4 +10,3 @@ let passport2 = {
 
 console.log(passport.name);
 console.log(passport2.name);
-//  ❗Для того, чтобы проверить правильность данного задания должно быть выведено 2 console.log
