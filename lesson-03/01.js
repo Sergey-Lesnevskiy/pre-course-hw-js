ca1d8d516d8fba786947f0cca7ffb873ad8dc75a
Lesnevsky-Sergey <78030028+Sergey-Lesnevskiy@users.noreply.github.com>
2024-06-03 21:15:45 +0300

    feat: add 1 task 3 week

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,10 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+let arr =  colors.filter((color)=>{
+  return color === 'черный'||color === 'красный'||color === 'желтый'
+});
+    return arr.join('-')
+}
+
+console.log(createColorString());
