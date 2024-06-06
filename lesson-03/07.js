3477aca67582e6eb2626e39ae2b4bf65135b8be4
Lesnevsky-Sergey <78030028+Sergey-Lesnevskiy@users.noreply.github.com>
2024-06-06 20:05:18 +0300

    fix 03-07 task

--- lesson-03/07.js
+++ lesson-03/07.js
@@ -9,7 +9,7 @@ const numbers = [
 
 for(let i = 0 ; i < numbers.length; i++){
   for(let j = 0 ; j < numbers[i].length; j++){
-    if(numbers[i][j] % 2 === 0){numbers[i].splice(j,1);}
+    if(numbers[i][j] % 2 !== 0){numbers[i].splice(j,1);}
   }
 }
 console.log(numbers);
